c1e27c421527aaacb8fb997d653d992cf24f733e
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-07 14:35:12 +0100

    feat: added sensorData Class

---
+++ lib/sensor_data.dart
@@ -0,0 +1,40 @@
+///Data Class for Sensor Data
+class SensorData {
+
+  ///Id for Objectbox generation
+  int id;
+
+  ///Datapoints that are saved.
+  late List<double> data;
+
+  ///Max precision of the values.
+  late int maxPrecision;
+
+  ///Id of the sensor.
+  late int sensorID;
+
+  ///Time the data got saved.
+  late DateTime dateTime;
+
+  ///Constructor for SensorData
+  SensorData({
+    required this.data,
+    required this.maxPrecision,
+    required this.sensorID,
+    this.id = 0,
+  }) {
+    dateTime = DateTime.now();
+  }
+
+  ///Returns SensorData
+  List<double> getData() => data;
+
+  ///Returns MaxPrecision
+  int getMaxPrecision() => maxPrecision;
+
+  ///Returns SensorID
+  int getSensorID() => sensorID;
+
+  ///Returns DateStamp of Sensor Data
+  DateTime getDateTime() => dateTime;
+}
