8460b788f9fa7b01b1b637167bcf76c1bc8d6098
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-13 16:10:12 +0100

    feat: updated buffer manager tests

--- test/buffer_manager_test.dart
+++ test/buffer_manager_test.dart
@@ -6,6 +6,17 @@ void main() {
   var bufferManager = BufferManager();
   var testData =
       SensorData(data: [1.1, 1.2, 1.3], maxPrecision: 1, sensorID: 1);
+  var testList =
+        [SensorData(data: [1.2, 1.2, 1.3], maxPrecision: 1, sensorID: 1)
+        ..dateTime = DateTime(2023),
+        SensorData(data: [2.2, 2.2, 3.3], maxPrecision: 1, sensorID: 1)
+        ..dateTime = DateTime(2022),
+        SensorData(data: [3.2, 3.2, 3.3], maxPrecision: 1, sensorID: 1)
+        ..dateTime = DateTime(2021),
+        SensorData(data: [4.2, 4.2, 4.3], maxPrecision: 1, sensorID: 1)
+        ..dateTime = DateTime(2020)
+        ];
+
   group("Basic testing of BufferManager", () {
     test("Add and Get Buffer from Buffermanager", () {
       bufferManager.addBuffer(1);
@@ -13,20 +24,32 @@ void main() {
     });
 
     test("Add buffer twice", () {
-      expect(() => bufferManager.addBuffer(1), throwsException);
+      bufferManager.addBuffer(2);
+      expect(() => bufferManager.addBuffer(2), throwsException);
     });
 
     test("Add Data to Buffer", () {
-      bufferManager.getBuffer(1).add(testData);
-      expect(bufferManager.getBuffer(1)[0], testData);
+      bufferManager.addBuffer(3);
+      bufferManager.getBuffer(3).add(testData);
+      expect(bufferManager.getBuffer(3)[0], testData);
     });
     test("Remove Data from Buffer", () {
-      bufferManager.getBuffer(1).remove(testData);
-      expect(bufferManager.getBuffer(1).isEmpty, true);
+      bufferManager
+        ..addBuffer(4)
+        ..getBuffer(4).add(testData);
+      bufferManager.getBuffer(4).remove(testData);
+      expect(bufferManager.getBuffer(4).isEmpty, true);
     });
     test("Remove buffer from buffer manager.", () {
-      bufferManager.removeBuffer(1);
-      expect(() => bufferManager.getBuffer(1), throwsException);
+      bufferManager..addBuffer(5)..removeBuffer(5);
+      expect(() => bufferManager.getBuffer(5), throwsException);
+    });
+    test("Test buffer sorting algorithm.", () {
+      bufferManager
+        ..addBuffer(6)
+        ..getBuffer(6).addAll(testList);
+      expect(bufferManager.getBuffer(6).last.dateTime.year,2023);
+      expect(bufferManager.getBuffer(6).first.dateTime.year,2020);
     });
   });
 }
