937ad3a10cc11d1c8b86e1cc0ac31d5e646291f9
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-13 20:21:39 +0100

    fix: updated SensorID to enum

--- test/buffer_manager_test.dart
+++ test/buffer_manager_test.dart
@@ -5,17 +5,32 @@ import 'package:smart_sensing_library/sensor_data.dart';
 
 void main() {
   var bufferManager = BufferManager();
-  var testData =
-      SensorData(data: [1.1, 1.2, 1.3], maxPrecision: 1, sensorID: 1);
+  var testData = SensorData(
+    data: [1.1, 1.2, 1.3],
+    maxPrecision: 1,
+    sensorID: SensorId.accelerometer,
+  );
   var testList = [
-    SensorData(data: [1.2, 1.2, 1.3], maxPrecision: 1, sensorID: 1)
-      ..dateTime = DateTime(2023),
-    SensorData(data: [2.2, 2.2, 3.3], maxPrecision: 1, sensorID: 1)
-      ..dateTime = DateTime(2022),
-    SensorData(data: [3.2, 3.2, 3.3], maxPrecision: 1, sensorID: 1)
-      ..dateTime = DateTime(2021),
-    SensorData(data: [4.2, 4.2, 4.3], maxPrecision: 1, sensorID: 1)
-      ..dateTime = DateTime(2020)
+    SensorData(
+      data: [1.2, 1.2, 1.3],
+      maxPrecision: 1,
+      sensorID: SensorId.accelerometer,
+    )..dateTime = DateTime(2023),
+    SensorData(
+      data: [2.2, 2.2, 3.3],
+      maxPrecision: 1,
+      sensorID: SensorId.accelerometer,
+    )..dateTime = DateTime(2022),
+    SensorData(
+      data: [3.2, 3.2, 3.3],
+      maxPrecision: 1,
+      sensorID: SensorId.accelerometer,
+    )..dateTime = DateTime(2021),
+    SensorData(
+      data: [4.2, 4.2, 4.3],
+      maxPrecision: 1,
+      sensorID: SensorId.accelerometer,
+    )..dateTime = DateTime(2020)
   ];
 
   group("Basic testing of BufferManager", () {
