000622d44b19fa6852e9c263e505a22526c17bde
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-23 12:20:08 +0100

    refactor: refactored database function

--- lib/io_manager.dart
+++ lib/io_manager.dart
@@ -8,7 +8,10 @@ import 'objectbox.g.dart';
 import 'sensor_data.dart';
 import 'sensor_data_dto.dart';
 
-/// This class is the main component of the smart sensing library
+/// This class is the core component of the smart sensing library.
+///
+/// The IOManager is the main access point for getting and saving sensor data.
+/// All other components are used/managed by the IOManager.
 class IOManager {
   IOManager._constructor() {
     _bufferManager = BufferManager();
@@ -21,65 +24,77 @@ class IOManager {
   ///Returns instance of IOManager
   factory IOManager() => _instance;
 
-  ///Opens the Database for access
+  ///Opens the database for access.
+  ///
+  ///Returns true when connection is established.
   Future<bool> openDatabase() async {
-    try {
-      await _openObjectboxDB();
-      return true;
-    } on Exception catch (e) {
-      print(e);
-      return false;
-    }
-  }
-
-  ///Adds a Sensor with [id].
-  void addSensor(SensorId id) {
-    _checkBufferSize(1);
-  }
-
-  ///Removes a Sensor with [id].
-  void removeSensor(SensorId id) {}
-
-  ///Gets Data from Database
-  Future<List<SensorData>> getFromDatabase(DateTime from, DateTime to) async {
-    if (_objectStore != null) {
-      var query = (_objectStore!.box<SensorDataDTO>().query(
-                SensorDataDTO_.id.equals(1).and(
-                      SensorDataDTO_.dateTime.between(
-                        from.millisecondsSinceEpoch,
-                        to.millisecondsSinceEpoch - 1,
-                      ),
-                    ),
-              )..order(SensorDataDTO_.dateTime, flags: Order.descending))
-          .build();
-      var list = query.find();
-      return list.map((e) => e.toSensorData()).toList();
-    }
-    return Future.error("Database connection is not established");
-  }
-
-  ///Saves Data from Database
-  Future<void> saveToDatabase(SensorId id) async {
-    if (_objectStore != null) {
-      var buffer = _bufferManager.getBuffer(id);
-      var dtoList = buffer.map(SensorDataDTO.fromSensorData).toList();
-      await _objectStore!.box<SensorDataDTO>().putManyAsync(dtoList);
-      buffer.clear();
-    }
-  }
-
-  bool _checkBufferSize(int size) => size >= _maxBufferSize;
-
-  Future<void> _openObjectboxDB() async {
     await getApplicationDocumentsDirectory().then(
       (dir) => {
         _objectStore = Store(
           getObjectBoxModel(),
-          directory: join(dir.path, 'my_databaseObjectBox.db'),
+          directory: join(dir.path, 'smart_sensing_library.db'),
         )
       },
     );
+    return true;
   }
+
+  ///Adds a Sensor with [id]. Works as a test function.
+  void addSensor(SensorId id) {
+    if (_objectStore == null) {
+      throw Exception(
+          "Database connection is not established!"
+          "Please first established to use the IOManager!");
+    }
+    _bufferManager.addBuffer(id);
+  }
+
+  ///Removes a Sensor with [id]. Works as a test function.
+  void removeSensor(SensorId id) {
+     if (_objectStore == null) {
+      throw Exception(
+          "Database connection is not established!"
+          "Please first established to use the IOManager!");
+    }
+    _bufferManager.removeBuffer(id);
+  }
+
+  ///Gets Data from Database
+  Future<List<SensorData>> getFromDatabase(
+    DateTime from,
+    DateTime to,
+    SensorId id,
+  ) async {
+    if (_objectStore == null) {
+      throw Exception("Database connection is not established!");
+    }
+    var query = (_objectStore!.box<SensorDataDTO>().query(
+              SensorDataDTO_.id.equals(id.index).and(
+                    SensorDataDTO_.dateTime.between(
+                      from.millisecondsSinceEpoch,
+                      to.millisecondsSinceEpoch - 1,
+                    ),
+                  ),
+            )..order(SensorDataDTO_.dateTime, flags: Order.descending))
+        .build();
+    var list = query.find();
+    return list.map((e) => e.toSensorData()).toList();
+  }
+
+  ///Saves Data from Database
+  Future<void> saveToDatabase(SensorId id) async {
+    if (_objectStore == null) {
+      throw Exception(
+          "Database connection is not established!"
+          "Please first established to use the IOManager!");
+    }
+    var buffer = _bufferManager.getBuffer(id);
+    var dtoList = buffer.map(SensorDataDTO.fromSensorData).toList();
+    await _objectStore!.box<SensorDataDTO>().putManyAsync(dtoList);
+    buffer.clear();
+  }
+
+  bool _checkBufferSize(int size) => size >= _maxBufferSize;
 
   ///Returns instance of FilterTool with corresponding buffer.
   Future<FilterTools?> getFilterFrom(
@@ -88,6 +103,11 @@ class IOManager {
     DateTime? to,
     Duration interval = Duration.zero,
   }) async {
+     if (_objectStore == null) {
+      throw Exception(
+          "Database connection is not established!"
+          "Please first established to use the IOManager!");
+    }
     from ??= DateTime.utc(-271821, 04, 20);
     to ??= DateTime.now();
     try {
@@ -96,7 +116,7 @@ class IOManager {
           buffer.last.dateTime.isAfter(to)) {
         return FilterTools(_splitWithDateTime(from, to, buffer));
       } else {
-        buffer = await getFromDatabase(from, to);
+        buffer = await getFromDatabase(from, to, id);
       }
       return FilterTools(buffer);
     } on Exception catch (e) {
