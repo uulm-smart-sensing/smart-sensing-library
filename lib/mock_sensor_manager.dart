f2f8adfe48baffd7d33ad8e995d6b37591c77b53
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-23 13:22:11 +0100

    feat: added mock Sensor Manager

--- lib/mock_sensor_manager.dart
+++ lib/mock_sensor_manager.dart
@@ -1,77 +1,36 @@
-import 'dart:async';
 import 'dart:collection';
 
 import 'package:sensing_plugin/sensing_plugin.dart';
 import 'sensor_data.dart';
 
 ///A SensorManager for testing purposes
-class MockSensorManager {
+class MockSensorManager{
+
   MockSensorManager._constructor();
   static final MockSensorManager _instance = MockSensorManager._constructor();
-  final HashMap _streamMap = HashMap<SensorId, StreamController<SensorData>?>();
+  final HashMap _streamMap =
+  HashMap<SensorId, Stream<SensorData>?>();
 
   ///Instance for MockSensorManager
   factory MockSensorManager() => _instance;
 
-  ///Creates a Stream for with [id].
-  ///
-  ///Returns a stream that gives back data every second.
-  Stream<SensorData> addSensor(SensorId id) =>
-      _createStream(const Duration(seconds: 1), id, 10);
+ ///Creates a Stream for testingPurposes
+  Stream<SensorData> addSensor(SensorId id) {
+    var stream =  Stream<SensorData>.periodic(
+          const Duration(seconds: 1), (x) => _createTestData(x, id),);
+    _streamMap[id] = stream;
+    return stream;
+  }
 
-  ///Removes the stream with [id].
-  Future<void> removeSensor(SensorId id) async {
-    await (_streamMap[id] as StreamController).close();
+  ///Creates a Stream for testingPurposes
+  void removeSensor(SensorId id) {
     _streamMap[id] = null;
   }
 
-  ///Creates test data.
   SensorData _createTestData(int i, SensorId id) => SensorData(
         data: [i + 0.1, i + 0.2, i + 0.3],
         maxPrecision: 2,
         sensorID: id,
       );
 
-  ///Creates a testing stream.
-  Stream<SensorData> _createStream(
-    Duration interval,
-    SensorId id, [
-    int? maxCount,
-  ]) {
-    late StreamController<SensorData> controller;
-    Timer? timer;
-    var counter = 0;
-
-    Future<void> tick(_) async {
-      counter++;
-      if (!controller.isClosed) {
-        controller.add(_createTestData(counter, id));
-        if (counter == maxCount) {
-          timer?.cancel();
-          await controller
-              .close(); // Ask stream to shut down and tell listeners.
-        }
-      } else {
-        timer?.cancel();
-      }
-    }
-
-    void startTimer() {
-      timer = Timer.periodic(interval, tick);
-    }
-
-    void stopTimer() {
-      timer?.cancel();
-      timer = null;
-    }
-
-    controller = StreamController<SensorData>(
-      onListen: startTimer,
-      onPause: stopTimer,
-      onResume: startTimer,
-      onCancel: stopTimer,
-    );
-    _streamMap[id] = controller;
-    return controller.stream;
-  }
 }
