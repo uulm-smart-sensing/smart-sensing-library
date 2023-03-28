93813f53ab53ea3321fb69784a4578a6bc9ecb6e
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-28 17:14:46 +0200

    chore: updated gitignore and setup for objectbox

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,11 @@
 flutter pub get
 flutter pub run build_runner build
 
+bash "<(curl -s https://raw.githubusercontent.com/objectbox/objectbox-dart/main/install.sh)"
+
+Remove-Item -path ./download/ -recurse
+
 Set-Location sensing-plugin
 .\setup.ps1
+
+
--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ flutter pub run build_runner build
 
 bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-dart/main/install.sh)
 
+rm -f -r download/
+
 cd sensing-plugin
 bash setup.sh
 
