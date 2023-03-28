a35519adbe55c5a08ee0ff8e228b964ca29a9057
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-03-28 14:37:49 +0200

    chore: updated scripts for objectbox

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 flutter pub get
 flutter pub run build_runner build
 
+bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-dart/main/install.sh)
+
 cd sensing-plugin
 bash setup.sh
+
+cd ..
