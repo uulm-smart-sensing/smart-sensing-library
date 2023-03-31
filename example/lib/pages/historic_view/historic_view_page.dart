2ced65afe2e06f77f711c6f032b57ad339623a6a
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-31 16:27:30 +0200

    refactor: Added SensorId parameter for sensor specific pages

--- example/lib/pages/historic_view/historic_view_page.dart
+++ example/lib/pages/historic_view/historic_view_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:smart_sensing_library/smart_sensing_library.dart';
 
 class HistoricViewPage extends StatelessWidget {
-  const HistoricViewPage({super.key});
+  final SensorId _sensorId;
+
+  const HistoricViewPage({
+    super.key,
+    required SensorId sensorId,
+  }) : _sensorId = sensorId;
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: const Text("Historic view"),
         ),
-        body: const Placeholder(),
+        body: Center(
+          child: Text("This is the historic view of ${_sensorId.name}"),
+        ),
       );
 }
