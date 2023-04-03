11a7aaf9965a2afea0f2872f9dacdaf159bbc31e
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-03 18:59:03 +0200

    feat: Added sensor information to DeviceWidget

--- example/lib/pages/home/device_widget.dart
+++ example/lib/pages/home/device_widget.dart
@@ -14,28 +14,77 @@ class _DeviceWidgetState extends State<DeviceWidget> {
   @override
   Widget build(BuildContext context) {
     var phoneIcon = Platform.isIOS ? Icons.phone_iphone : Icons.phone_android;
-    var header = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        _getDeviceNameTitle(),
-        Icon(
-          phoneIcon,
-          color: Colors.white,
-        ),
-      ],
+    var header = Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _getDeviceNameTitle(),
+          Icon(
+            phoneIcon,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+
+    var horizontalPadding = 16.0;
+    var rowPadding = 8.0;
+    var body = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Table(
+        columnWidths: const <int, TableColumnWidth>{
+          0: IntrinsicColumnWidth(),
+          1: IntrinsicColumnWidth(),
+          2: FlexColumnWidth(),
+        },
+        children: [
+          TableRow(
+            children: [
+              _getAvailableSensorsText(),
+              SizedBox(width: horizontalPadding),
+              const Text(
+                "sensors available",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          // Padding row
+          TableRow(
+            children: [
+              SizedBox(height: rowPadding),
+              SizedBox(height: rowPadding),
+              SizedBox(height: rowPadding),
+            ],
+          ),
+          TableRow(
+            children: [
+              _getRunningSensorsText(),
+              SizedBox(width: horizontalPadding),
+              const Text(
+                "sensors running",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
 
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(15)),
         color: Color.fromARGB(255, 170, 77, 255),
       ),
-      width: 320,
-      height: 80,
       child: Column(
         children: [
           header,
+          body,
         ],
       ),
     );
@@ -56,7 +105,13 @@ Widget _getDeviceNameTitle() => FutureBuilder(
       }),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Text(snapshot.data!);
+          return Text(
+            snapshot.data!,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+            ),
+          );
         }
 
         if (snapshot.hasError) {
@@ -64,5 +119,63 @@ Widget _getDeviceNameTitle() => FutureBuilder(
         }
 
         return const Text("Device name is being fetched ...");
+      },
+    );
+
+/// Fetches the number of available sensors and builds a [Text] widget with it.
+Widget _getAvailableSensorsText() => FutureBuilder(
+      // ignore: prefer_expression_function_bodies
+      future: Future.sync(() async {
+        // ignore: todo
+        // TODO: Replace with call to smart sensing library
+        return "14";
+      }),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(
+            snapshot.data!,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Text(
+            "Error occured while fetching number of available sensors",
+          );
+        }
+
+        return const Text("Number of available sensors is being fetched ...");
+      },
+    );
+
+/// Fetches the number of running sensors and builds a [Text] widget with it.
+Widget _getRunningSensorsText() => FutureBuilder(
+      // ignore: prefer_expression_function_bodies
+      future: Future.sync(() async {
+        // ignore: todo
+        // TODO: Replace with call to smart sensing library
+        return "10";
+      }),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(
+            snapshot.data!,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Text(
+            "Error occured while fetching number of running sensors",
+          );
+        }
+
+        return const Text("Number of running sensors is being fetched ...");
       },
     );
