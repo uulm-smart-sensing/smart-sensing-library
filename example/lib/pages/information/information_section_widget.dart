a576c86c20903098eb2c556d17ae9268d8c794b4
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-04-04 11:52:15 +0200

    feat: created widget for displaying information of a certain topic, e. g. the 'License'

---
+++ example/lib/pages/information/information_section_widget.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class InformationSectionWidget extends StatelessWidget {
+  /// The title of the section.
+  ///
+  /// For example 'License'.
+  final String sectionTitle;
+
+  /// The [content] of this information section, which
+  /// should be only [Text] or [ListView], but can contain
+  /// more complex text constructions too.
+  final Widget content;
+
+  const InformationSectionWidget({
+    super.key,
+    required this.sectionTitle,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Display the text, which represents the "title" of
+    // this information section
+    var header = Text(
+      sectionTitle,
+      textAlign: TextAlign.left,
+      style: const TextStyle(fontSize: 24),
+    );
+
+    // Display the information (content) in a rounded, dynamic sized box
+    var body = Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Color.fromARGB(255, 207, 149, 218),
+      ),
+      child: content,
+    );
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          header,
+          const SizedBox(
+            height: 10,
+          ),
+          body,
+        ],
+      ),
+    );
+  }
+}
