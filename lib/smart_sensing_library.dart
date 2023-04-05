27fbff4176f4b9e655788c3395ebf724b94ab33f
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-04-05 04:19:13 +0200

    feat: exposed IOManager

--- lib/smart_sensing_library.dart
+++ lib/smart_sensing_library.dart
@@ -5,4 +5,6 @@ export 'package:sensing_plugin/sensing_plugin.dart' show SensorId;
 export 'package:smart_sensing_library/device_information_manager.dart'
     show getOSVersion, getFreeStorage;
 
+export 'package:smart_sensing_library/io_manager.dart' show IOManager;
+
 // TODO: Add API methods
