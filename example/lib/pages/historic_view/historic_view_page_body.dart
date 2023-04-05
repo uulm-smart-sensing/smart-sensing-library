7b7c2592f0efacae2220910b94b6ca38b09392d1
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-05 12:51:45 +0200

    feat: Added visualization table

--- example/lib/pages/historic_view/historic_view_page_body.dart
+++ example/lib/pages/historic_view/historic_view_page_body.dart
@@ -3,11 +3,16 @@ import 'package:smart_sensing_library/smart_sensing_library.dart';
 
 import '../../general_widgets/stylized_container.dart';
 
-enum Filter {
+enum _Filter {
   noFilter,
   count,
   mode,
   min,
+}
+
+enum _Visualization {
+  table,
+  graph,
 }
 
 class HistoricViewPageBody extends StatefulWidget {
@@ -20,8 +25,9 @@ class HistoricViewPageBody extends StatefulWidget {
 }
 
 class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
-  var selectedFilter = Filter.noFilter;
+  var selectedFilter = _Filter.noFilter;
   var selectedDuration = const Duration(minutes: 5);
+  var selectedVisualization = _Visualization.table;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +52,8 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
                 });
               },
               title: "5 min",
+              isSelected:
+                  selectedDuration.compareTo(const Duration(minutes: 5)) == 0,
             ),
             divider,
             _getTimeSelectionButton(
@@ -55,6 +63,8 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
                 });
               },
               title: "1 h",
+              isSelected:
+                  selectedDuration.compareTo(const Duration(hours: 1)) == 0,
             ),
             divider,
             _getTimeSelectionButton(
@@ -64,6 +74,8 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
                 });
               },
               title: "12 h",
+              isSelected:
+                  selectedDuration.compareTo(const Duration(hours: 12)) == 0,
             ),
             divider,
             _getTimeSelectionButton(
@@ -73,6 +85,8 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
                 });
               },
               title: "2 d",
+              isSelected:
+                  selectedDuration.compareTo(const Duration(days: 2)) == 0,
             ),
             divider,
             _getTimeSelectionButton(
@@ -82,12 +96,16 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
                 });
               },
               title: "1 w",
+              isSelected:
+                  selectedDuration.compareTo(const Duration(days: 7)) == 0,
             ),
           ],
         ),
       ),
     );
 
+    // Selection between different filters
+    // When filter is selected, new filter will be applied to table/graph
     var filterSelectionDropdown = StylizedContainer(
       padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
       width: double.infinity,
@@ -103,12 +121,12 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
         items: const [
           DropdownMenuItem(
             enabled: false,
-            value: Filter.noFilter,
+            value: _Filter.noFilter,
             child: Text("Please select a filter"),
           ),
-          DropdownMenuItem(value: Filter.count, child: Text("Count")),
-          DropdownMenuItem(value: Filter.mode, child: Text("Mode")),
-          DropdownMenuItem(value: Filter.min, child: Text("Min")),
+          DropdownMenuItem(value: _Filter.count, child: Text("Count")),
+          DropdownMenuItem(value: _Filter.mode, child: Text("Mode")),
+          DropdownMenuItem(value: _Filter.min, child: Text("Min")),
         ],
         onChanged: (newFilter) {
           if (newFilter == null) {
@@ -123,25 +141,142 @@ class _HistoricViewPageBodyState extends State<HistoricViewPageBody> {
       ),
     );
 
+    // Selection between different visualizations (graph and table)
+    // When visualization is selected, according visualization is shown
+    var visualizationSelection = IntrinsicHeight(
+      child: Row(
+        children: [
+          _getVisualizationSelectionButton(
+            onPressed: () {
+              setState(() {
+                selectedVisualization = _Visualization.graph;
+              });
+            },
+            title: "Graph",
+            isSelected: selectedVisualization == _Visualization.graph,
+          ),
+          divider,
+          _getVisualizationSelectionButton(
+            onPressed: () {
+              setState(() {
+                selectedVisualization = _Visualization.table;
+              });
+            },
+            title: "Table",
+            isSelected: selectedVisualization == _Visualization.table,
+          ),
+        ],
+      ),
+    );
+
+    List<Text> xyzElements;
+    if (selectedVisualization == _Visualization.graph) {
+      xyzElements = [
+        const Text(
+          "X",
+          style: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+        const Text(
+          "Y",
+          style: TextStyle(
+            color: Colors.green,
+          ),
+        ),
+        const Text(
+          "Z",
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+      ];
+    } else {
+      xyzElements = [
+        const Text("X"),
+        const Text("Y"),
+        const Text("Z"),
+      ];
+    }
+
+    var visualizationTable = Table(
+      columnWidths: const <int, TableColumnWidth>{
+        0: FlexColumnWidth(3),
+        1: FlexColumnWidth(),
+        2: FlexColumnWidth(),
+        3: FlexColumnWidth(),
+      },
+      children: [
+        TableRow(
+          children: [
+            visualizationSelection,
+            ...xyzElements,
+          ],
+        ),
+        TableRow(
+          children: [
+            visualizationSelection,
+            ...xyzElements,
+          ],
+        ),
+      ],
+    );
+
     return Column(
       children: [
         timeIntervalSelection,
         const SizedBox(height: 10),
         filterSelectionDropdown,
+        const SizedBox(height: 20),
+        visualizationTable,
       ],
     );
   }
 }
 
+Widget _getSelectionButton({
+  required double width,
+  required double height,
+  required void Function() onPressed,
+  required String title,
+  required bool isSelected,
+}) =>
+    SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: TextStyle(
+            decoration: isSelected ? TextDecoration.underline : null,
+          ),
+        ),
+      ),
+    );
+
 Widget _getTimeSelectionButton({
   required void Function() onPressed,
   required String title,
+  required bool isSelected,
 }) =>
-    SizedBox(
+    _getSelectionButton(
       width: 50,
       height: 35,
-      child: TextButton(
-        onPressed: onPressed,
-        child: Text(title),
-      ),
+      onPressed: onPressed,
+      title: title,
+      isSelected: isSelected,
+    );
+
+Widget _getVisualizationSelectionButton({
+  required void Function() onPressed,
+  required String title,
+  required bool isSelected,
+}) =>
+    _getSelectionButton(
+      width: 50,
+      height: 20,
+      onPressed: onPressed,
+      title: title,
+      isSelected: isSelected,
     );
