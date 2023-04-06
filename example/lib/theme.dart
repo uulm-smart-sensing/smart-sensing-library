d90368524923724958e5fe962f6a4496be62a3f2
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-07 01:27:12 +0200

    refactor: Moved theme to separate file

--- example/lib/theme.dart
+++ example/lib/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_sensing_library/smart_sensing_library.dart';
 
+/// Unique [Color] for each [SensorId].
 const sensorIdToColor = {
   SensorId.accelerometer: Color.fromARGB(255, 162, 255, 105),
   SensorId.gyroscope: Color.fromARGB(255, 107, 252, 182),
@@ -11,4 +12,51 @@ const sensorIdToColor = {
   SensorId.thermometer: Color.fromARGB(255, 252, 168, 107),
 };
 
-// TODO: add theme here as constant
+const primaryColorHex = 0xFF00072F;
+const primaryColor = MaterialColor(
+  primaryColorHex,
+  {
+    50: Color.fromRGBO(0, 7, 47, .1),
+    100: Color.fromRGBO(0, 7, 47, .2),
+    200: Color.fromRGBO(0, 7, 47, .3),
+    300: Color.fromRGBO(0, 7, 47, .4),
+    400: Color.fromRGBO(0, 7, 47, .5),
+    500: Color.fromRGBO(0, 7, 47, .6),
+    600: Color.fromRGBO(0, 7, 47, .7),
+    700: Color.fromRGBO(0, 7, 47, .8),
+    800: Color.fromRGBO(0, 7, 47, .9),
+    900: Color.fromRGBO(0, 7, 47, 1),
+  },
+);
+
+/// Theme used for the app-
+var theme = ThemeData(
+  primarySwatch: primaryColor,
+  scaffoldBackgroundColor: const Color(primaryColorHex),
+  dividerColor: Colors.white,
+  cardColor: const Color.fromARGB(255, 34, 0, 77),
+  iconTheme: const IconThemeData(
+    color: Colors.white,
+  ),
+  textTheme: const TextTheme(
+    // Style used for Text widget
+    bodyMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 18,
+    ),
+    // Style used for DropdownButton text
+    titleMedium: TextStyle(
+      fontSize: 18,
+      color: Colors.white,
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: Colors.white,
+      padding: EdgeInsets.zero,
+      textStyle: const TextStyle(
+        fontSize: 18,
+      ),
+    ),
+  ),
+);
