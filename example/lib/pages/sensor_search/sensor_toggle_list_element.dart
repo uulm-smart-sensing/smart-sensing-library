70cccb2bed373725521da732a269b45d7e133e5e
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-07 00:49:31 +0200

    refactor: SensorToggleListElement is now using key

--- example/lib/pages/sensor_search/sensor_toggle_list_element.dart
+++ example/lib/pages/sensor_search/sensor_toggle_list_element.dart
@@ -4,39 +4,29 @@ import 'package:smart_sensing_library/smart_sensing_library.dart';
 import '../../utils.dart';
 
 class SensorToggleListElement extends StatefulWidget {
-  final SensorId _sensorId;
-  final Function(bool) _onChanged;
-  final Color? _color;
-  final bool? _isToggledOn;
-  final Color? _activeColor;
-  final Color? _activeTrackColor;
-  final Color? _inactiveColor;
-  final Color? _inactiveTrackColor;
-  final Color? _textColor;
-  final double? _fontSize;
+  final SensorId sensorId;
+  final Function(bool) onChanged;
+  final Color? color;
+  final bool? isToggledOn;
+  final Color? activeColor;
+  final Color? activeTrackColor;
+  final Color? inactiveColor;
+  final Color? inactiveTrackColor;
+  final Color? textColor;
+  final double? fontSize;
 
-  const SensorToggleListElement({
-    super.key,
-    required SensorId sensorId,
-    required Function(bool) onChanged,
-    Color? color,
-    bool? isToggledOn,
-    Color? activeColor,
-    Color? activeTrackColor,
-    Color? inactiveColor,
-    Color? inactiveTrackColor,
-    Color? textColor,
-    double? fontSize,
-  })  : _sensorId = sensorId,
-        _onChanged = onChanged,
-        _color = color,
-        _isToggledOn = isToggledOn,
-        _activeColor = activeColor,
-        _activeTrackColor = activeTrackColor,
-        _inactiveColor = inactiveColor,
-        _inactiveTrackColor = inactiveTrackColor,
-        _textColor = textColor,
-        _fontSize = fontSize;
+  SensorToggleListElement({
+    required this.sensorId,
+    required this.onChanged,
+    this.color,
+    this.isToggledOn,
+    this.activeColor,
+    this.activeTrackColor,
+    this.inactiveColor,
+    this.inactiveTrackColor,
+    this.textColor,
+    this.fontSize,
+  }) : super(key: ValueKey(sensorId.name));
 
   @override
   State<SensorToggleListElement> createState() =>
@@ -48,14 +38,14 @@ class _SensorToggleListElementState extends State<SensorToggleListElement> {
 
   @override
   void initState() {
-    _isToggledOn = widget._isToggledOn;
+    _isToggledOn = widget.isToggledOn;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => Container(
         decoration: BoxDecoration(
-          color: widget._color,
+          color: widget.color,
           borderRadius: BorderRadius.circular(20),
         ),
         child: Row(
@@ -66,8 +56,8 @@ class _SensorToggleListElementState extends State<SensorToggleListElement> {
               child: Text(
                 formatPascalCase(widget._sensorId.name),
                 style: TextStyle(
-                  color: widget._textColor,
-                  fontSize: widget._fontSize,
+                  color: widget.textColor,
+                  fontSize: widget.fontSize,
                 ),
               ),
             ),
@@ -78,15 +68,15 @@ class _SensorToggleListElementState extends State<SensorToggleListElement> {
                 child: Switch.adaptive(
                   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                   value: _isToggledOn ?? false,
-                  activeColor: widget._activeColor,
-                  activeTrackColor: widget._activeTrackColor,
-                  inactiveThumbColor: widget._inactiveColor,
-                  inactiveTrackColor: widget._inactiveTrackColor,
+                  activeColor: widget.activeColor,
+                  activeTrackColor: widget.activeTrackColor,
+                  inactiveThumbColor: widget.inactiveColor,
+                  inactiveTrackColor: widget.inactiveTrackColor,
                   onChanged: (isToggledOn) {
                     setState(() {
                       _isToggledOn = isToggledOn;
                     });
-                    widget._onChanged.call(isToggledOn);
+                    widget.onChanged.call(isToggledOn);
                   },
                 ),
               ),
