04f19350a90671b4bef6eeaf41ffcb7a247c3251
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-04-06 11:55:42 +0200

    docs: added further comments to the 'Import / Export' page

--- example/lib/pages/import_export/import_export_page.dart
+++ example/lib/pages/import_export/import_export_page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
 import '../../general_widgets/smart_sensing_appbar.dart';
+import '../settings/settings_page.dart';
 import 'import_export_section_widget.dart';
 
+/// Page for importing and exporting sensor data into / from the smart
+/// sensing library.
+///
+/// This [ImportExportPage] shows two sections:
+/// * the first section is for importing sensor data,
+/// so contains a [DropdownButton] and [TextButton] to
+/// select for which sensor new data should be imported.
+/// * the second section is for exporting sensor data,
+/// so contains a [DropdownButton] and [TextButton]s to
+/// select whose sensor data should be exported at which time interval
+/// ("All" - meaning all collected and saved sensor data and "Manual" -
+/// meaning the user can select a time interval restricting the sensor data,
+/// which should be exported)
+///
+/// To select the files where to sensor data should be stored / load,
+/// a [FilePicker] is used, so the user can easily select, which file should
+/// be used.
+///
+/// This [ImportExportPage] is reachable through the [SettingsPage].
 class ImportExportPage extends StatelessWidget {
   const ImportExportPage({super.key});
 
@@ -14,6 +34,8 @@ class ImportExportPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // The widget (= section) containing all information and buttons
+    // for allowing the user to import sensor data
     var importContainer = ImportExportSectionWidget(
       sectionTitle: "Import sensor data",
       buttons: SizedBox(
@@ -25,6 +47,8 @@ class ImportExportPage extends StatelessWidget {
       ),
     );
 
+    // The widget (= section) containing all information and buttons
+    // for allowing the user to export sensor data
     var exportContainer = ImportExportSectionWidget(
       sectionTitle: "Export sensor data",
       buttons: Column(
