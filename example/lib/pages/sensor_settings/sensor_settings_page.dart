b0a6b134a78623749b550c8ab28eb1a3d2b5c18f
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-11 18:33:12 +0200

    feat: Added basic layout

--- example/lib/pages/sensor_settings/sensor_settings_page.dart
+++ example/lib/pages/sensor_settings/sensor_settings_page.dart
@@ -1,21 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:smart_sensing_library/smart_sensing_library.dart';
 
+import '../../general_widgets/smart_sensing_appbar.dart';
+
 class SensorSettingsPage extends StatelessWidget {
-  final SensorId _sensorId;
+  final SensorId sensorId;
 
   const SensorSettingsPage({
     super.key,
-    required SensorId sensorId,
-  }) : _sensorId = sensorId;
+    required this.sensorId,
+  });
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("Sensor settings"),
+  Widget build(BuildContext context) {
+    var unitHeader = const Text(
+      "devices",
+      style: TextStyle(
+        fontSize: 24,
+      ),
+    );
+    var unitSelection = Container(
+
+    );
+
+    var precisionHeader = const Text(
+      "Precision",
+      style: TextStyle(
+        fontSize: 24,
+      ),
+    );
+    var precisionSelection = Container(
+
+    );
+
+    var timeIntervalHeader = const Text(
+      "Time Interval (h:m:s)",
+      style: TextStyle(
+        fontSize: 24,
+      ),
+    );
+    var timeIntervalSelection = Container(
+      // TODO: use time selection from demo maybe
+    );
+
+    var applyButton = Container(
+      width: 100,
+      height: 40,
+      child: const Placeholder(),
+    );
+
+    return SmartSensingAppBar(
+      title: "Sensor settings",
+      // TODO: Capitalize
+      subtitle: sensorId.name,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                unitHeader,
+                unitSelection,
+                precisionHeader,
+                precisionSelection,
+                timeIntervalHeader,
+                timeIntervalSelection,
+              ],
+            ),
+            applyButton,
+          ],
         ),
-        body: Center(
-          child: Text("These are the sensor settings of ${_sensorId.name}"),
-        ),
-      );
+      ),
+    );
+  }
 }
