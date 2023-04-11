dd9a7900fb5936ae6c64bb17a7ce477655e4b28c
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-11 19:06:56 +0200

    feat: Added unit string representation

---
+++ example/lib/unit_string_representation.dart
@@ -0,0 +1,18 @@
+import 'package:smart_sensing_library/smart_sensing_library.dart';
+
+const unitToUnitStringRepresentation = {
+  Unit.metersPerSecondSquared: "m/s²",
+  Unit.gravitationalForce: "G",
+  Unit.radiansPerSecond: "radians/s",
+  Unit.degreesPerSecond: "degress/s",
+  Unit.microTeslas: "µT",
+  Unit.radians: "radians",
+  Unit.degrees: "degress",
+  Unit.hectoPascal: "hPa",
+  Unit.kiloPascal: "kPa",
+  Unit.bar: "Bar",
+  Unit.celsius: "°C",
+  Unit.fahrenheit: "°F",
+  Unit.kelvin: "K",
+  Unit.unitless: "",
+};
