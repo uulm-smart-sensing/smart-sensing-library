3a6610c7b8434bf1c03eb1b24bc81624fb78cbd2
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-17 00:28:43 +0200

    docs: Added documentation

--- example/lib/general_widgets/custom_dropdown_button.dart
+++ example/lib/general_widgets/custom_dropdown_button.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'stylized_container.dart';
 
+/// A customized [DropdownButton] that is wrapped by a [StylizedContainer].
+///
+/// The customization includes:
+/// * taking the full width of the parent widget
+/// * both the [DropdownButton] and the [DropdownMenu] have a rounded shape and
+/// the purple theme color
+/// * [Icons.keyboard_arrow_down_rounded] is used as icon
 class CustomDropdownButton<T> extends StylizedContainer {
+  /// The text being displayed when no [DropdownMenuItem] is selected / when
+  /// [value] is null.
   final String hint;
+  /// The value of the currently selected [DropdownMenuItem].
+  ///
+  /// When [value] is null, i.e. no [DropdownMenuItem] is selected, the [hint]
+  /// is displayed.
   final T value;
+  /// The list of items the user can choose from when pressing this button.
   final List<DropdownMenuItem<T>> items;
+  /// Called when the user selects an item.
   final void Function(T? newValue) onChanged;
+  /// Reduces the height of this button.
   final bool isDense;
 
   CustomDropdownButton({
