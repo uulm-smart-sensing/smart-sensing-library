cf2099422895c87781bdc4af6e4d4cfcf3d2cfc2
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-04-17 01:33:44 +0200

    feat: better formating of box and unit

--- example/lib/general_widgets/brick_live_information_container.dart
+++ example/lib/general_widgets/brick_live_information_container.dart
@@ -52,9 +52,16 @@ class _BrickLiveInformation extends StatefulWidget {
 }
 
 class _BrickLiveInformationState extends State<_BrickLiveInformation> {
+  var style = const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    fontFamily: "Ubuntu",
+  );
+
   Duration lastUpdate = Duration.zero;
   DateTime lastTimeStamp = DateTime.now().toUtc();
   List<double?> mainData = [];
+  Unit? unit;
   @override
   void initState() {
     super.initState();
@@ -62,10 +69,12 @@ class _BrickLiveInformationState extends State<_BrickLiveInformation> {
       (event) {
         setState(() {
           var tmpDateTime = DateTime.fromMicrosecondsSinceEpoch(
-              event.timestampInMicroseconds);
+            event.timestampInMicroseconds,
+          );
           lastUpdate = tmpDateTime.difference(lastTimeStamp);
           lastTimeStamp = tmpDateTime;
           mainData = event.data;
+          unit ??= event.unit;
         });
       },
     );
@@ -81,42 +90,73 @@ class _BrickLiveInformationState extends State<_BrickLiveInformation> {
               children: [
                 Text(
                   widget.headLine,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                    fontFamily: "Ubuntu",
-                  ),
+                  style: style,
                 ),
                 const Spacer(),
                 widget.icon,
               ],
             ),
-            Text(_fromData(mainData)),
-            const Text(
+            Text(
+              () {
+                var string = _fromData(mainData, unit ?? Unit.unitless);
+                if (string.isNotEmpty) {
+                  return string;
+                }
+                return "No Data";
+              }(),
+              style: style,
+            ),
+            Text(
               "Latest update:",
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-                fontFamily: "Ubuntu",
-              ),
+              style: style,
             ),
             Text(
               lastUpdate.toString(),
-              style: const TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-                fontFamily: "Ubuntu",
-              ),
+              style: style,
             ),
           ],
         ),
       );
 }
 
-String _fromData(List<double?> data) {
+String _fromData(List<double?> data, Unit unit) {
   var result = "";
   for (var element in data) {
-    result += element?.toString() ?? "";
+    result +=
+        ": ${element?.toStringAsFixed(3) ?? ""} ${_unitConverter(unit)}\n";
   }
   return result;
+}
+
+String _unitConverter(Unit unit) {
+  switch (unit) {
+    case Unit.metersPerSecondSquared:
+      return "m/s";
+    case Unit.gravitationalForce:
+      return "m/s^2";
+    case Unit.radiansPerSecond:
+      return "rad/s";
+    case Unit.degreesPerSecond:
+      return "deg/s";
+    case Unit.microTeslas:
+      return "µT";
+    case Unit.radians:
+      return "rad";
+    case Unit.degrees:
+      return "deg";
+    case Unit.hectoPascal:
+      return "hPa";
+    case Unit.kiloPascal:
+      return "kPa";
+    case Unit.bar:
+      return "bar";
+    case Unit.celsius:
+      return "°C";
+    case Unit.fahrenheit:
+      return "°F";
+    case Unit.kelvin:
+      return "K";
+    case Unit.unitless:
+      return "";
+  }
 }
