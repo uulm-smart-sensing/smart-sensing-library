10c6822a5d204e7fc886c9ed81e786f4279b8a90
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-18 23:02:07 +0200

    feat: Added Live view page

--- example/lib/pages/live_view/live_view_page.dart
+++ example/lib/pages/live_view/live_view_page.dart
@@ -1,13 +1,57 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:smart_sensing_library/smart_sensing_library.dart';
+
+import '../../date_formatter.dart';
+import '../../general_widgets/smart_sensing_appbar.dart';
+import '../../theme.dart';
+import '../historic_view/historic_view_page.dart';
 
 class LiveViewPage extends StatelessWidget {
   const LiveViewPage({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("Live view"),
-        ),
-        body: const Placeholder(),
-      );
+  Widget build(BuildContext context) {
+    var gridView = GridView.count(
+      crossAxisCount: 2,
+      physics: const BouncingScrollPhysics(),
+      children: SensorId.values
+          .map(
+            (id) => Padding(
+              padding: const EdgeInsets.all(12),
+              // TODO: Replace with sensor widget
+              child: GestureDetector(
+                child: Container(
+                  color: sensorIdToColor[id]!.withAlpha(128),
+                  child: Placeholder(
+                    color: sensorIdToColor[id]!,
+                    child: Center(child: Text(id.name)),
+                  ),
+                ),
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HistoricViewPage(sensorId: id),
+                  ),
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
+
+    return SmartSensingAppBar(
+      title: "Live view",
+      subtitle: formatDate(
+        dateTime: DateTime.now(),
+        locale: Platform.localeName,
+        extendWithDayName: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: gridView,
+      ),
+    );
+  }
 }
