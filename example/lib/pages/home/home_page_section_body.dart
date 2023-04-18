5c303894b9702e9e359f578ba4f9129af6a3fa91
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-18 23:37:28 +0200

    refactor: Refactored HomePageSectionBody to use height of children widgets

--- example/lib/pages/home/home_page_section_body.dart
+++ example/lib/pages/home/home_page_section_body.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// List along a specified [Axis] in which widgets are displayed.
 class HomePageSectionBody extends StatelessWidget {
   /// Axis on which the children are displayed and scrolled.
   ///
-  /// Default value is [Axis.vertical].
+  /// Default value is [Axis.horizontal].
   final Axis scrollDirection;
 
   /// Padding space between each [Widget] in [children].
   ///
-  /// Default value is zero.
+  /// Default value is 20.
   final double spaceBetweenChildren;
-
-  /// Determines whether this [HomePageSectionBody] should have a fixed size or
-  /// take the remaining size of the parent [Widget].
-  ///
-  /// If this is `true`, [size] has to be set, otherwise [size] will be ignored.
-  final bool hasFixedSize;
-
-  /// Size of this [HomePageSectionBody].
-  ///
-  /// The size applies only to the other opposite [Axis] from [scrollDirection],
-  /// i.e. when [scrollDirection] is [Axis.horizontal] the size will be applied
-  /// to the vertical axis and vice versa.
-  final double size;
 
   /// [Widget]s which are listed in this [HomePageSectionBody].
   ///
@@ -32,10 +20,8 @@ class HomePageSectionBody extends StatelessWidget {
 
   const HomePageSectionBody({
     super.key,
-    this.scrollDirection = Axis.vertical,
-    this.spaceBetweenChildren = 0,
-    this.hasFixedSize = false,
-    this.size = 0,
+    this.scrollDirection = Axis.horizontal,
+    this.spaceBetweenChildren = 20,
     this.children = const [],
   });
 
@@ -54,19 +40,10 @@ class HomePageSectionBody extends StatelessWidget {
       }
     }
 
-    // This only works using Container:
-    // ignore: sized_box_for_whitespace
-    return Container(
-      height: hasFixedSize && scrollDirection == Axis.horizontal ? size : null,
-      width: hasFixedSize && scrollDirection == Axis.vertical ? size : null,
-      child: Expanded(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: scrollDirection,
-          children: bodyElements,
-        ),
-      ),
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: scrollDirection,
+      child: Row(children: bodyElements),
     );
   }
 }
