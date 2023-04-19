bd055d7e43a9ac11d293a875f06242a3e698af66
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-19 18:16:44 +0200

    feat: Updated addSensor

--- lib/smart_sensing_library.dart
+++ lib/smart_sensing_library.dart
@@ -1,7 +1,7 @@
 library smart_sensing_library;
 
 export 'package:sensing_plugin/sensing_plugin.dart'
-    show SensorId, SensorInfo, Unit, SensorAccuracy;
+    show SensorId, SensorInfo, Unit, SensorAccuracy, SensorTaskResult;
 
 export 'package:smart_sensing_library/device_information_manager.dart'
     show getOSVersion, getFreeStorage, getDeviceName;
