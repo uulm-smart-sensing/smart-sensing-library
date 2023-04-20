a9883052f5c5cb941e311e430f5054b7be7a4977
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-04-21 01:01:35 +0200

    feat: exposed sensor config

--- lib/smart_sensing_library.dart
+++ lib/smart_sensing_library.dart
@@ -1,8 +1,14 @@
 library smart_sensing_library;
 
 export 'package:sensing_plugin/sensing_plugin.dart'
-    show SensorId, SensorInfo, Unit, SensorAccuracy, SensorData,
-     SensorTaskResult;
+    show
+        SensorId,
+        SensorInfo,
+        Unit,
+        SensorAccuracy,
+        SensorData,
+        SensorTaskResult,
+        SensorConfig;
 
 export 'package:smart_sensing_library/device_information_manager.dart'
     show getOSVersion, getFreeStorage, getDeviceName;
