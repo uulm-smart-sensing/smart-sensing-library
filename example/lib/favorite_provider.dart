665996c905738d917d3d7fe60816256436dae75a
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-22 01:52:15 +0200

    feat: added shared preference to FavoriteProvider

--- example/lib/favorite_provider.dart
+++ example/lib/favorite_provider.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/foundation.dart';
 import 'package:smart_sensing_library/smart_sensing_library.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+/// Class that handles Favorite Managment
 class FavoriteProvider extends ChangeNotifier {
+  /// List of all favorite Sensors
   List<SensorId> _sensorList = [];
   List<SensorId> get sensorList => _sensorList;
 
-  void toggleFavorite(SensorId id) {
+  final Future<SharedPreferences> _prefs;
+
+  // Constructor that initialises the SharedPreferences and loads the
+  //list of favourites.
+  FavoriteProvider() : _prefs = SharedPreferences.getInstance() {
+    loadFavorites();
+  }
+
+  /// adds SensorId to sensorList if it has not already been added
+  Future<void> toggleFavorite(SensorId id) async {
     var isExist = _sensorList.contains(id);
     if (isExist) {
       _sensorList.remove(id);
@@ -13,13 +25,32 @@ class FavoriteProvider extends ChangeNotifier {
       _sensorList.add(id);
     }
     notifyListeners();
+
+    var pref = await _prefs;
+    await pref.setStringList(
+      'favoriteSensors',
+      _sensorList.map((id) => id.index.toString()).toList(),
+    );
   }
 
-  bool isExist(SensorId id) => _sensorList.contains(id);
-  bool isSlidableEnabled(SensorId id) => id != SensorId.barometer;
-  bool isEmpty() => _sensorList.isEmpty;
-  void clearFavorite() {
-    _sensorList = [];
+  /// loads the list of favorite Sensors from SharedPreferences
+  Future<void> loadFavorites() async {
+    var pref = await _prefs;
+    var favoriteSensors = pref.getStringList('favoriteSensors') ?? [];
+
+    _sensorList = favoriteSensors
+        .map((sensor) => SensorId.values[int.parse(sensor)])
+        .toList();
+
     notifyListeners();
   }
+
+  /// checks if SensorId exists in List
+  bool isExist(SensorId id) => _sensorList.contains(id);
+
+  /// checks if SesnorId is slideable
+  bool isSlidableEnabled(SensorId id) => id != SensorId.barometer;
+
+  /// checks if sensorList is empty
+  bool isEmpty() => _sensorList.isEmpty;
 }
