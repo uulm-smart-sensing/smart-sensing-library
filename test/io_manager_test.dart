9a36d852cceff2dee4616e2d1fd9dfb3d841d417
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-04-25 16:50:30 +0200

    feat: added Sensormanager reset

--- test/io_manager_test.dart
+++ test/io_manager_test.dart
@@ -23,7 +23,7 @@ Future<void> main() async {
   );
 
   setUp(
-    () => ioManager.removeData(SensorId.accelerometer).then(
+    () => FakeSensorManager().resetState().then(
           (value) => ioManager.removeSensor(SensorId.accelerometer),
         ),
   );
