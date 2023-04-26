14cbbca187ef94404ac80b97c6ac5a66674aa0b4
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-26 10:05:37 +0200

    refactor: changed class name & modified graph view

--- example/lib/pages/historic_view/graph_view.dart
+++ example/lib/pages/historic_view/graph_view.dart
@@ -5,9 +5,10 @@ import 'sensor_data.dart';
 
 /// Widget for displaying Graph in Historic View
 class GraphView extends StatelessWidget {
-  //variable that determines how many data points are needed
-  final int lineData;
-  const GraphView({super.key, required this.lineData});
+  /// Variable that determines how many data points are needed
+  final int listOfDataPoints;
+
+  const GraphView({super.key, required this.listOfDataPoints});
 
   @override
   Widget build(BuildContext context) => LineChart(
@@ -16,7 +17,7 @@ class GraphView extends StatelessWidget {
           gridData: gridData,
           titlesData: titleData,
           borderData: borderData,
-          lineBarsData: chartData(lineData),
+          lineBarsData: chartData(listOfDataPoints),
           minX: getMinTimeStamp(testData),
           maxX: getMaxTimeStamp(testData),
           minY: 0,
@@ -25,7 +26,7 @@ class GraphView extends StatelessWidget {
       );
 }
 
-/// determines whether there will be 3 or 1 linechart depending on the parameter
+/// Determines whether there will be 3 or 1 linechart depending on the parameter
 List<LineChartBarData> chartData(int quantity) {
   if (quantity != 3) {
     return [lineChartBarDataX];
@@ -35,7 +36,7 @@ List<LineChartBarData> chartData(int quantity) {
 
 FlGridData get gridData => FlGridData(show: false);
 
-/// changes border attribute for the graph
+/// Changes border attribute for the graph
 FlBorderData get borderData => FlBorderData(
       show: true,
       border: Border.all(
@@ -44,7 +45,7 @@ FlBorderData get borderData => FlBorderData(
       ),
     );
 
-/// handles the touch event from the graph
+/// Handles the touch event from the graph
 LineTouchData get lineTouchData => LineTouchData(
       handleBuiltInTouches: true,
       getTouchLineStart: (data, index) => 0,
@@ -61,7 +62,7 @@ LineTouchData get lineTouchData => LineTouchData(
           );
           var date = DateTime.fromMillisecondsSinceEpoch(touchedSpot.x.toInt());
           return LineTooltipItem(
-            "${'${DateFormat('HH:mm').format(date)} Uhr'}\n"
+            "${'${DateFormat('HH:mm').format(date)} '}\n"
             "${touchedSpot.y.toStringAsFixed(2)}",
             textStyle,
           );
@@ -69,40 +70,34 @@ LineTouchData get lineTouchData => LineTouchData(
       ),
     );
 
-/// handles the data from Graph
-LineChartBarData get lineChartBarDataX => LineChartBarData(
-      color: Colors.red,
-      isStrokeCapRound: true,
+/// Handles the data from Graph
+LineChartBarData _getLineChartBarData(Color color, List<FlSpot> spots) =>
+    LineChartBarData(
+      color: color,
       dotData: FlDotData(show: false),
       belowBarData: BarAreaData(show: false),
       barWidth: 3,
       isCurved: true,
-      spots: testData.map((data) => FlSpot(data.timestamp, data.x)).toList(),
+      spots: spots,
     );
 
-LineChartBarData get lineChartBarDataY => LineChartBarData(
-      color: Colors.green,
-      isStrokeCapRound: true,
-      dotData: FlDotData(show: false),
-      belowBarData: BarAreaData(show: false),
-      barWidth: 3,
-      isCurved: true,
-      spots: testData.map((data) => FlSpot(data.timestamp, data.y!)).toList(),
+LineChartBarData get lineChartBarDataX => _getLineChartBarData(
+      Colors.red,
+      testData.map((data) => FlSpot(data.timestamp, data.x)).toList(),
     );
 
-LineChartBarData get lineChartBarDataZ => LineChartBarData(
-      color: Colors.blue,
-      isStrokeCapRound: true,
-      dotData: FlDotData(show: false),
-      belowBarData: BarAreaData(show: false),
-      barWidth: 3,
-      isCurved: true,
-      spots: testData.map((data) => FlSpot(data.timestamp, data.z!)).toList(),
+LineChartBarData get lineChartBarDataY => _getLineChartBarData(
+      Colors.green,
+      testData.map((data) => FlSpot(data.timestamp, data.y!)).toList(),
     );
 
-/// handles the titles
+LineChartBarData get lineChartBarDataZ => _getLineChartBarData(
+      Colors.blue,
+      testData.map((data) => FlSpot(data.timestamp, data.z!)).toList(),
+    );
+
+/// Handles the titles
 FlTitlesData get titleData => FlTitlesData(
-      show: true,
       bottomTitles: AxisTitles(sideTitles: bottomTitles),
       leftTitles: AxisTitles(sideTitles: leftTitles),
       rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
@@ -124,7 +119,7 @@ Widget bottomTitleWidgets(double value, TitleMeta meta) {
   );
   Widget text;
   var date = DateTime.fromMillisecondsSinceEpoch(value.toInt());
-  text = Text('${DateFormat('HH:mm').format(date)}\n Uhr', style: style);
+  text = Text('${DateFormat('HH:mm').format(date)}\n ', style: style);
 
   return SideTitleWidget(
     axisSide: meta.axisSide,
