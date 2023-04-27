ca838dd1e8e90208934f0d1a23de1c256df14cf1
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-27 21:19:41 +0200

    refactor: changed Graphview to a statefulwidget

--- example/lib/pages/historic_view/graph_view.dart
+++ example/lib/pages/historic_view/graph_view.dart
@@ -4,145 +4,159 @@ import 'package:intl/intl.dart';
 import 'sensor_view_data.dart';
 
 /// Widget for displaying Graph in Historic View
-class GraphView extends LineChart {
-  /// Variable that determines how many data points are needed
+/// Widget for displaying Graph in Historic View
+///
+class GraphView extends StatefulWidget {
   final int lineDataCount;
   final List<SensorViewData> lineData;
+  const GraphView({
+    super.key,
+    required this.lineData,
+    required this.lineDataCount,
+  });
 
-  GraphView({super.key, required this.lineDataCount, required this.lineData})
-      : super(
-          LineChartData(
-            lineTouchData: lineTouchData,
-            gridData: gridData,
-            titlesData: titleData,
-            borderData: borderData,
-            lineBarsData: chartData(lineDataCount),
-            minX: getMinTimeStamp(lineData),
-            maxX: getMaxTimeStamp(lineData),
-            minY: 0,
-            maxY: getMaxUnit(lineData) + 1,
-          ),
-        );
+  @override
+  State<GraphView> createState() => _GraphView();
 }
 
-/// Determines whether there will be 3 or 1 linechart depending on the parameter
-List<LineChartBarData> chartData(int quantity) {
-  if (quantity != 3) {
-    return [lineChartBarDataX];
+class _GraphView extends State<GraphView> {
+  //variable that determines how many data points are needed
+
+  @override
+  Widget build(BuildContext context) => LineChart(
+        LineChartData(
+          lineTouchData: lineTouchData,
+          gridData: gridData,
+          titlesData: titleData,
+          borderData: borderData,
+          lineBarsData: chartData(widget.lineDataCount),
+          minX: getMinX(widget.lineData),
+          maxX: getMaxX(widget.lineData),
+          minY: getMinY(widget.lineData),
+          maxY: getMaxY(widget.lineData) + 1,
+        ),
+      );
+
+  /// Determines whether there will be 3 or 1 linechart depending on the parameter
+  List<LineChartBarData> chartData(int quantity) {
+    if (quantity != 3) {
+      return [lineChartBarDataX];
+    }
+    return [lineChartBarDataX, lineChartBarDataY, lineChartBarDataZ];
   }
-  return [lineChartBarDataX, lineChartBarDataY, lineChartBarDataZ];
-}
 
-FlGridData get gridData => FlGridData(show: false);
+  LineChartBarData get lineChartBarDataX => _getLineChartBarData(
+        Colors.red,
+        widget.lineData.map((data) => FlSpot(data.timestamp, data.x)).toList(),
+      );
 
-/// Changes border attribute for the graph
-FlBorderData get borderData => FlBorderData(
-      border: Border.all(
-        color: Colors.white,
-        width: 1,
-      ),
+  LineChartBarData get lineChartBarDataY => _getLineChartBarData(
+        Colors.green,
+        widget.lineData.map((data) => FlSpot(data.timestamp, data.y!)).toList(),
+      );
+
+  LineChartBarData get lineChartBarDataZ => _getLineChartBarData(
+        Colors.blue,
+        widget.lineData.map((data) => FlSpot(data.timestamp, data.z!)).toList(),
+      );
+  FlGridData get gridData => FlGridData(show: false);
+
+  /// Changes border attribute for the graph
+  FlBorderData get borderData => FlBorderData(
+        border: Border.all(
+          color: Colors.white,
+          width: 1,
+        ),
+      );
+
+  /// Handles the touch event from the graph
+  LineTouchData get lineTouchData => LineTouchData(
+        handleBuiltInTouches: true,
+        getTouchLineStart: (data, index) => 0,
+        touchTooltipData: LineTouchTooltipData(
+          fitInsideHorizontally: true,
+          fitInsideVertically: true,
+          maxContentWidth: 100,
+          tooltipBgColor: Colors.black.withOpacity(0.8),
+          getTooltipItems: (touchedSpots) => touchedSpots.map((touchedSpot) {
+            var textStyle = TextStyle(
+              color:
+                  touchedSpot.bar.gradient?.colors[0] ?? touchedSpot.bar.color,
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            );
+            var date =
+                DateTime.fromMillisecondsSinceEpoch(touchedSpot.x.toInt());
+            return LineTooltipItem(
+              "${'${DateFormat('HH:mm').format(date)} '}\n"
+              "${touchedSpot.y.toStringAsFixed(2)}",
+              textStyle,
+            );
+          }).toList(),
+        ),
+      );
+
+  /// Handles the data from Graph
+  LineChartBarData _getLineChartBarData(Color color, List<FlSpot> spots) =>
+      LineChartBarData(
+        color: color,
+        dotData: FlDotData(show: false),
+        belowBarData: BarAreaData(show: false),
+        barWidth: 3,
+        //isCurved: true,
+        spots: spots,
+      );
+
+  /// Handles the titles
+  FlTitlesData get titleData => FlTitlesData(
+        bottomTitles: AxisTitles(sideTitles: bottomTitles),
+        leftTitles: AxisTitles(sideTitles: leftTitles),
+        rightTitles: AxisTitles(sideTitles: SideTitles()),
+        topTitles: AxisTitles(sideTitles: SideTitles()),
+      );
+
+  SideTitles get bottomTitles => SideTitles(
+        showTitles: true,
+        reservedSize: 30,
+        getTitlesWidget: bottomTitleWidgets,
+        interval: getMaxX(widget.lineData) - getMinX(widget.lineData) / 2,
+      );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 12,
     );
+    Widget text;
+    var date = DateTime.fromMillisecondsSinceEpoch(value.toInt());
+    text = Text('${DateFormat('HH:mm').format(date)}\n ', style: style);
 
-/// Handles the touch event from the graph
-LineTouchData get lineTouchData => LineTouchData(
-      handleBuiltInTouches: true,
-      getTouchLineStart: (data, index) => 0,
-      touchTooltipData: LineTouchTooltipData(
-        fitInsideHorizontally: true,
-        fitInsideVertically: true,
-        maxContentWidth: 100,
-        tooltipBgColor: Colors.black.withOpacity(0.8),
-        getTooltipItems: (touchedSpots) => touchedSpots.map((touchedSpot) {
-          var textStyle = TextStyle(
-            color: touchedSpot.bar.gradient?.colors[0] ?? touchedSpot.bar.color,
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-          );
-          var date = DateTime.fromMillisecondsSinceEpoch(touchedSpot.x.toInt());
-          return LineTooltipItem(
-            "${'${DateFormat('HH:mm').format(date)} '}\n"
-            "${touchedSpot.y.toStringAsFixed(2)}",
-            textStyle,
-          );
-        }).toList(),
-      ),
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: text,
     );
+  }
 
-/// Handles the data from Graph
-LineChartBarData _getLineChartBarData(Color color, List<FlSpot> spots) =>
-    LineChartBarData(
-      color: color,
-      dotData: FlDotData(show: false),
-      belowBarData: BarAreaData(show: false),
-      barWidth: 3,
-      isCurved: true,
-      spots: spots,
+  SideTitles get leftTitles => SideTitles(
+        showTitles: true,
+        reservedSize: 40,
+        getTitlesWidget: leftTitleWidgets,
+        interval: getMaxY(widget.lineData) / 2.5,
+      );
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
     );
-
-LineChartBarData get lineChartBarDataX => _getLineChartBarData(
-      Colors.red,
-      testData.map((data) => FlSpot(data.timestamp, data.x)).toList(),
+    var newValue = value.round();
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text('$newValue', style: style),
     );
-
-LineChartBarData get lineChartBarDataY => _getLineChartBarData(
-      Colors.green,
-      testData.map((data) => FlSpot(data.timestamp, data.y!)).toList(),
-    );
-
-LineChartBarData get lineChartBarDataZ => _getLineChartBarData(
-      Colors.blue,
-      testData.map((data) => FlSpot(data.timestamp, data.z!)).toList(),
-    );
-
-/// Handles the titles
-FlTitlesData get titleData => FlTitlesData(
-      bottomTitles: AxisTitles(sideTitles: bottomTitles),
-      leftTitles: AxisTitles(sideTitles: leftTitles),
-      rightTitles: AxisTitles(sideTitles: SideTitles()),
-      topTitles: AxisTitles(sideTitles: SideTitles()),
-    );
-
-SideTitles get bottomTitles => SideTitles(
-      showTitles: true,
-      reservedSize: 30,
-      getTitlesWidget: bottomTitleWidgets,
-      interval: (getMaxTimeStamp(testData) - getMinTimeStamp(testData)) / 2,
-    );
-
-Widget bottomTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    color: Colors.white,
-    fontWeight: FontWeight.bold,
-    fontSize: 12,
-  );
-  Widget text;
-  var date = DateTime.fromMillisecondsSinceEpoch(value.toInt());
-  text = Text('${DateFormat('HH:mm').format(date)}\n ', style: style);
-
-  return SideTitleWidget(
-    axisSide: meta.axisSide,
-    space: 4,
-    child: text,
-  );
-}
-
-SideTitles get leftTitles => SideTitles(
-      showTitles: true,
-      reservedSize: 40,
-      getTitlesWidget: leftTitleWidgets,
-      interval: getMaxUnit(testData) / 2.5,
-    );
-
-Widget leftTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    color: Colors.white,
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  );
-  var newValue = value.round();
-  return SideTitleWidget(
-    axisSide: meta.axisSide,
-    space: 4,
-    child: Text('$newValue', style: style),
-  );
+  }
 }
