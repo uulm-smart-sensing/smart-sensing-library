029fc75db30eb4e3bf022aa6517637ace293f6ac
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-05-14 19:01:58 +0200

    test: modified test result

--- test/export_test.dart
+++ test/export_test.dart
@@ -78,7 +78,7 @@ Future<void> main() async {
         SupportedFileFormat.json,
         [SensorId.accelerometer],
       );
-      expect(wasExportSuccessful2, [ExportResult.formattedDataEmpty]);
+      expect(wasExportSuccessful2, isTrue);
     });
 
     test("Export only work with at least one sensor (id)", () async {
@@ -94,7 +94,7 @@ Future<void> main() async {
         SupportedFileFormat.json,
         [SensorId.accelerometer],
       );
-      expect(wasExportSuccessful2, [ExportResult.succesful]);
+      expect(wasExportSuccessful2, [ExportResult.successful]);
     });
 
     test("Export only works, if sensor data really exist.", () async {
@@ -110,7 +110,7 @@ Future<void> main() async {
         SupportedFileFormat.json,
         [SensorId.accelerometer],
       );
-      expect(wasExportSuccessful2, [ExportResult.succesful]);
+      expect(wasExportSuccessful2, [ExportResult.successful]);
     });
 
     test(
@@ -131,7 +131,7 @@ Future<void> main() async {
         [SensorId.accelerometer],
         DateTime.now().add(const Duration(seconds: -10)),
       );
-      expect(wasExportSuccessful2, [ExportResult.succesful]);
+      expect(wasExportSuccessful2, [ExportResult.successful]);
     });
 
     test(
