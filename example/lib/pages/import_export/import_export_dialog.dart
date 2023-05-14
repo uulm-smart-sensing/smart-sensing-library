a0a00b8f20734d1f69554d6932d1536c99074cdf
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-05-14 13:44:06 +0200

    feat: added an alertdialog, an alertmessages which prints the result and exportsresults list.

--- example/lib/pages/import_export/import_export_dialog.dart
+++ example/lib/pages/import_export/import_export_dialog.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:smart_sensing_library/io_manager.dart';
 
-class ImportExportDialog extends StatelessWidget {
-  final String title;
-  final ListView content;
-  const ImportExportDialog(
-      {super.key, required this.title, required this.content});
-
-  @override
-  Widget build(BuildContext context) => AlertDialog(
-        title: Text(title),
-        content: SingleChildScrollView(
-          child: content,
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text("Done"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          )
-        ],
-      );
+var exportResults = <ExportResult>[];
+void showExportDialog(
+  BuildContext context,
+  String title,
+  List<ExportResult> content,
+) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      backgroundColor: Theme.of(context).primaryColor,
+      title: Text(title, style: const TextStyle(color: Colors.white)),
+      content: Text(
+        alertmessages(content),
+        style: const TextStyle(color: Colors.white),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text("Done"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    ),
+  );
 }
 
-String alertmessages(ExportResult result) {
-  switch (result) {
-    case ExportResult.succesful:
-      return "Data was sucessfully exported";
-    case ExportResult.directoryNotExists:
-      return "Directory does not exists";
-    case ExportResult.sensorIdEmpty:
-      return "SensorId is empty";
-    case ExportResult.formattedDataEmpty:
-      return "Formatted Data is Empty";
+String alertmessages(List<ExportResult> results) {
+  var message = "";
+  for (var result in results) {
+    switch (result) {
+      case ExportResult.succesful:
+        message += "Data was successfully exported\n";
+        break;
+      case ExportResult.directoryNotExists:
+        message += "Directory does not exists\n";
+        break;
+      case ExportResult.sensorIdEmpty:
+        message += "SensorId is empty\n";
+        break;
+      case ExportResult.formattedDataEmpty:
+        message += "Formatted Data is Empty\n";
+        break;
+      case ExportResult.notselectedDirectory:
+        message += "Not selected a Directory\n";
+        break;
+    }
   }
+  results.clear();
+  return message;
 }
