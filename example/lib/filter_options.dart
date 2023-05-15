70f5f155bc8b2a1e439de84a91a1197c15aa8af5
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-15 21:02:25 +0200

    feat: added more filter Options

--- example/lib/filter_options.dart
+++ example/lib/filter_options.dart
@@ -2,7 +2,12 @@ enum FilterOption {
   max(axisNumber: 3, shortText: "Max"),
   min(axisNumber: 3, shortText: "Min"),
   avg(axisNumber: 1, shortText: "Avg"),
-  sd(axisNumber: 1, shortText: "Standard dev.");
+  sd(axisNumber: 1, shortText: "Standard dev."),
+  mode(axisNumber: 3, shortText: "Mode"),
+  range(axisNumber: 1, shortText: "Range"),
+  median(axisNumber: 1, shortText: "Median"),
+  sum(axisNumber: 1, shortText: "Sum"),
+  ;
 
   const FilterOption({
     required this.axisNumber,
