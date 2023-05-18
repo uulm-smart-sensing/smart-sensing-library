118a1f0c7541fbcd6cc9a0bb1d905d4f0d4aefaf
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-05-18 16:34:03 +0200

    test: removed unecessary group name

--- example/test/favorite_provider_test.dart
+++ example/test/favorite_provider_test.dart
@@ -4,32 +4,29 @@ import 'package:smart_sensing_library/smart_sensing_library.dart';
 import 'package:smart_sensing_library_example/favorite_provider.dart';
 
 void main() {
-  group('toggleFavorite', () {
-    test('Adds a sensor to the favorites list if it is not available',
-        () async {
-      SharedPreferences.setMockInitialValues({});
-      var favoriteProvider = FavoriteProvider();
-      await favoriteProvider.loadFavorites();
-      var sensorId = SensorId.accelerometer;
+  test('Adds a sensor to the favorites list if it is not available', () async {
+    SharedPreferences.setMockInitialValues({});
+    var favoriteProvider = FavoriteProvider();
+    await favoriteProvider.loadFavorites();
+    var sensorId = SensorId.accelerometer;
 
-      await favoriteProvider.toggleFavorite(sensorId);
+    await favoriteProvider.toggleFavorite(sensorId);
 
-      expect(favoriteProvider.isExist(sensorId), isTrue);
-    });
+    expect(favoriteProvider.isExist(sensorId), isTrue);
+  });
 
-    test('Removes a sensor from the favorites list if it already exists',
-        () async {
-      SharedPreferences.setMockInitialValues({});
-      var favoriteProvider = FavoriteProvider();
-      await favoriteProvider.loadFavorites();
-      var sensorId = SensorId.accelerometer;
-      await favoriteProvider.toggleFavorite(sensorId);
+  test('Removes a sensor from the favorites list if it already exists',
+      () async {
+    SharedPreferences.setMockInitialValues({});
+    var favoriteProvider = FavoriteProvider();
+    await favoriteProvider.loadFavorites();
+    var sensorId = SensorId.accelerometer;
+    await favoriteProvider.toggleFavorite(sensorId);
 
-      expect(favoriteProvider.isExist(sensorId), isTrue);
+    expect(favoriteProvider.isExist(sensorId), isTrue);
 
-      await favoriteProvider.toggleFavorite(sensorId);
+    await favoriteProvider.toggleFavorite(sensorId);
 
-      expect(favoriteProvider.isExist(sensorId), isFalse);
-    });
+    expect(favoriteProvider.isExist(sensorId), isFalse);
   });
 }
