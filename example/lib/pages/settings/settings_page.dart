540dfc14a293c39416be55a719114646ca1d52f1
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-19 18:09:50 +0200

    feat: added routing for preview settings

--- example/lib/pages/settings/settings_page.dart
+++ example/lib/pages/settings/settings_page.dart
@@ -5,8 +5,6 @@ import '../home/home_page.dart';
 import '../import_export/import_export_page.dart';
 import '../information/information_page.dart';
 import '../preview_settings/preview_settings_page.dart';
-import '../license_page/app_license_page.dart';
-import '../preview_settings/preview_settings_page.dart';
 import '../sensor_search/sensor_search_page.dart';
 import 'settings_widget.dart';
 
