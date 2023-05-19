1e7d0b83c0e959b9f4b7f23215fe263a633d804c
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-19 20:13:08 +0200

    feat: added json conversion

--- example/lib/pages/preview_settings/sensor_preview_settings.dart
+++ example/lib/pages/preview_settings/sensor_preview_settings.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import '../../filter_options.dart';
 
 /// Class to save preview settings.
 class SensorPreviewSetting {
-  Duration timeInterval;
+  late Duration timeInterval;
   late final Map<FilterOption, bool> active;
   SensorPreviewSetting({
     this.timeInterval = const Duration(seconds: 5),
@@ -19,4 +21,17 @@ class SensorPreviewSetting {
     }
     return map;
   }
+
+  SensorPreviewSetting.fromJson(String jsonString) {
+    var json = jsonDecode(jsonString);
+    timeInterval = Duration(microseconds: json['timeInterval']);
+    active = (json['activeMap'] as Map<dynamic,dynamic>).cast<String,bool>().map(
+      (key, value) =>
+    MapEntry(FilterOption.values[int.parse(key)], value),);
+  }
+
+  String toJson() => json.encode({
+    'timeInterval': timeInterval.inMicroseconds,
+    'activeMap': active.map((key, value) => MapEntry(key.index.toString(), value)),
+  });
 }
