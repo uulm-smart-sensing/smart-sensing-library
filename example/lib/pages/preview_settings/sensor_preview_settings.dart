c06902ec8054a28bfcb110bdb09d5ff216c250a1
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-19 18:09:34 +0200

    feat: added sensor preview setting

--- example/lib/pages/preview_settings/sensor_preview_settings.dart
+++ example/lib/pages/preview_settings/sensor_preview_settings.dart
@@ -1,10 +1,163 @@
-import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:smart_sensing_library/smart_sensing_library.dart';
 
 import '../../filter_options.dart';
+import '../../formatter/text_formatter.dart';
+import '../../general_widgets/brick_container.dart';
+import '../../general_widgets/custom_text_button.dart';
+import '../../general_widgets/section_header.dart';
+import '../../general_widgets/smart_sensing_appbar.dart';
+import '../../sensor_units.dart';
+import '../../theme.dart';
+import '../sensor_settings/time_interval_selection_button.dart';
 
-/// Class to save preview settings.
+/// Page to configure the sensor preview with the passed [sensorId].
+///
+/// The settings which can be configured are:
+/// * filter of sensor data
+/// * target unit of sensor data
+/// * time interval of sensor update events
+class SensorPreviewSettingsPage extends StatefulWidget {
+  final SensorId sensorId;
+  const SensorPreviewSettingsPage({
+    super.key,
+    required this.sensorId,
+  });
+
+  @override
+  State<SensorPreviewSettingsPage> createState() =>
+      _SensorPreviewSettingsPageState();
+}
+
+class _SensorPreviewSettingsPageState extends State<SensorPreviewSettingsPage> {
+  late int selectedPrecision;
+  late int selectedTimeIntervalInMilliseconds;
+  late Unit selectedUnit;
+  late SensorPreviewSetting sensorSettings;
+
+  @override
+  void initState() {
+    selectedTimeIntervalInMilliseconds = 100;
+    selectedUnit = getUnitsFromSensorId(widget.sensorId).first;
+    sensorSettings = SensorPreviewSetting();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var filterHeader = SectionHeader("Filters");
+    var filterSelection = Expanded(
+      flex: 1,
+      child: _getFilterSliderFromData(),
+    );
+
+    var timeIntervalHeader = SectionHeader(
+      "Time Interval (m : s : ms)",
+    );
+    var timeIntervalSelection = Align(
+      alignment: Alignment.topCenter,
+      child: TimeIntervalSelectionButton(
+        timeIntervalInMilliseconds: selectedTimeIntervalInMilliseconds,
+        onChanged: (newValue) {
+          setState(() {
+            selectedTimeIntervalInMilliseconds = newValue;
+          });
+        },
+      ),
+    );
+
+    var applyButton = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: CustomTextButton(
+        isDense: false,
+        width: 200,
+        text: "Apply settings",
+        style: const TextStyle(
+          fontSize: 24,
+        ),
+        onPressed: () {
+          // TODO: Validate settings
+          // TODO: Apply settings
+        },
+      ),
+    );
+
+    return SmartSensingAppBar(
+      title: "Preview settings",
+      subtitle: formatPascalCase(widget.sensorId.name),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  filterHeader,
+                  filterSelection,
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        timeIntervalHeader,
+                        timeIntervalSelection,
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            applyButton,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getFilterSliderFromData() {
+    var itemList = <Widget>[];
+
+    for (var filter in FilterOption.values) {
+      var textContainer = Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Text(
+          formatPascalCase(filter.shortText),
+        ),
+      );
+      itemList.add(
+        BrickContainer(
+          color: sensorSettings.active[filter]!
+              ? activeTrackColor
+              : inactiveTrackColor,
+          alignment: Alignment.center,
+          onClick: () {
+            setState(() {
+              if (sensorSettings.active.containsKey(filter)) {
+                sensorSettings.active[filter] = !sensorSettings.active[filter]!;
+              } else {
+                sensorSettings.active[filter] = false;
+              }
+            });
+          },
+          child: textContainer,
+        ),
+      );
+    }
+
+    return GridView.count(
+      crossAxisCount: 3,
+      childAspectRatio: 1.5,
+      padding: const EdgeInsets.all(5),
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      shrinkWrap: true,
+      children: itemList,
+    );
+  }
+}
+
 class SensorPreviewSetting {
-  late Duration timeInterval;
+  final Duration timeInterval;
   late final Map<FilterOption, bool> active;
   SensorPreviewSetting({
     this.timeInterval = const Duration(seconds: 5),
@@ -21,20 +174,4 @@ class SensorPreviewSetting {
     }
     return map;
   }
-
-  SensorPreviewSetting.fromJson(String jsonString) {
-    var json = jsonDecode(jsonString);
-    timeInterval = Duration(microseconds: json['timeInterval']);
-    active =
-        (json['activeMap'] as Map<dynamic, dynamic>).cast<String, bool>().map(
-              (key, value) =>
-                  MapEntry(FilterOption.values[int.parse(key)], value),
-            );
-  }
-
-  String toJson() => json.encode({
-        'timeInterval': timeInterval.inMicroseconds,
-        'activeMap':
-            active.map((key, value) => MapEntry(key.index.toString(), value)),
-      });
 }
