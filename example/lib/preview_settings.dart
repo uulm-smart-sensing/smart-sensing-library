ff77b1ab4d8802d1753ad86f00b66a52f749b7f5
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-19 23:16:15 +0200

    refactor: rename and refactor to normal class

--- example/lib/preview_settings.dart
+++ example/lib/preview_settings.dart
@@ -6,7 +6,7 @@ import 'pages/preview_settings/sensor_preview_settings.dart';
 /// Handles the Preview settings management.
 /// The class uses the preference shared by the database. With shared preference
 /// it is possible to store the data globally and to modify it.
-class PreviewSettings {
+class PreviewSettings{
   /// List of all preview settings
   final Map<SensorId, SensorPreviewSetting> _sensorPreviewSetting = {};
   Map<SensorId, SensorPreviewSetting> get sensorPreviewSettings =>
@@ -15,37 +15,31 @@ class PreviewSettings {
   static SharedPreferences? _prefs;
   static PreviewSettings? _instance;
 
-  /// Private singleton constructor to initialize [SharedPreferences]
-  /// and init. [SensorPreviewSetting].
+  /// Initializes the `SharedPreferences` and loads the
+  /// previously stored list of favorites.
   PreviewSettings._create() {
     loadPreviewSettings();
   }
 
-  /// Factory Method for getting [PreviewSettings].
-  ///
-  /// Will only create one instance of [PreviewSettings]
-  /// and returns only that afterwards.
   static Future<PreviewSettings> getProvider() async {
     _prefs ??= await SharedPreferences.getInstance();
     _instance ??= PreviewSettings._create();
     return _instance!;
   }
 
-  /// Updates the [SensorPreviewSetting] of sensor [sensorId] with [settings].
-  ///
-  /// Gets updated in [SharedPreferences] afterwards.
+  /// Adds or removes a sensor[sensorId] to the [_sensorPreviewSetting] depending on whether
+  /// the sensor (with [sensorId]) was already been added (= remove) or not (= add).
   Future<void> updateSensorPreviewSettings(
     SensorId sensorId,
     SensorPreviewSetting settings,
   ) async {
-    _sensorPreviewSetting[sensorId] = settings;
     await _prefs!.setStringList(
       'previewSetting_$sensorId',
       [settings.toJson()],
     );
   }
 
-  /// Loads the Map of [SensorPreviewSetting] from [SharedPreferences].
+  /// Loads the list of favorite sensors from SharedPreferences
   void loadPreviewSettings() {
     for (var sensorId in SensorId.values) {
       var previewSettings = _prefs!.getStringList('previewSetting_$sensorId');
