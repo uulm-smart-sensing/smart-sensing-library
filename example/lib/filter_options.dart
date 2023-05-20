353f0dfc38b1d4e7e9f65b38ce62ca600494d05a
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-20 12:58:55 +0200

    docs: added docs to enum

--- example/lib/filter_options.dart
+++ example/lib/filter_options.dart
@@ -1,11 +1,22 @@
+import 'package:smart_sensing_library/filter_tools.dart';
+
+/// Enum that represents ever [FilterOption] of [FilterTools].
 enum FilterOption {
+  /// Represents the maximum filter from [FilterTools].
   max(axisNumber: 3, shortText: "Max"),
+  /// Represents the minimum filter from [FilterTools].
   min(axisNumber: 3, shortText: "Min"),
+  /// Represents the average filter from [FilterTools].
   avg(axisNumber: 1, shortText: "Avg"),
+  /// Represents the standard deviation filter from [FilterTools].
   sd(axisNumber: 1, shortText: "Standard dev."),
+  /// Represents the mode filter from [FilterTools].
   mode(axisNumber: 3, shortText: "Mode"),
+  /// Represents the range filter from [FilterTools].
   range(axisNumber: 1, shortText: "Range"),
+  /// Represents the median filter from [FilterTools].
   median(axisNumber: 1, shortText: "Median"),
+  /// Represents the sum filter from [FilterTools].
   sum(axisNumber: 1, shortText: "Sum"),
   ;
 
