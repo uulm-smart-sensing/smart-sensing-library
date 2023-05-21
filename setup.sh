c5d7f455cd6359079c79501a3b38a30ff8eefe02
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-05-21 17:24:52 +0200

    chore: Added submodule initialization into setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-dart/main/install.sh)
-
-flutter pub get
-flutter pub run build_runner build
-
-rm -f -r download/
+git submodule update --init
 
 cd sensing-plugin
 bash setup.sh
+
+bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-dart/main/install.sh)
+
+rm -f -r download/
+
+flutter pub get
+flutter pub run build_runner build
