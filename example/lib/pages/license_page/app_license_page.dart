dd69c54a64a729cd311e61eca7447606ea133885
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-05-22 00:36:33 +0200

    style: Fixed formatting

--- example/lib/pages/license_page/app_license_page.dart
+++ example/lib/pages/license_page/app_license_page.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppLicensePage extends StatelessWidget {
-const AppLicensePage({ super.key });
+  const AppLicensePage({super.key});
 
   @override
   Widget build(BuildContext context) => Theme(
-    data: Theme.of(context),
-    child: LicensePage(
-      applicationName: "Smart Sensing Library App",
-      applicationVersion: "0.0.0",
-      applicationIcon: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: FlutterLogo(),
-      ),
-      applicationLegalese: "© ${DateTime.now().year} Smart Sensing Library",
-    ),
-  );
+        data: Theme.of(context),
+        child: LicensePage(
+          applicationName: "Smart Sensing Library App",
+          applicationVersion: "0.0.0",
+          applicationIcon: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: FlutterLogo(),
+          ),
+          applicationLegalese: "© ${DateTime.now().year} Smart Sensing Library",
+        ),
+      );
 }
