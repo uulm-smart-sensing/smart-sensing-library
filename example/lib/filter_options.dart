e7758fac176ee498c48453258985181090d0078b
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-21 15:44:05 +0200

    docs: typo and addition

--- example/lib/filter_options.dart
+++ example/lib/filter_options.dart
@@ -1,46 +1,46 @@
 import 'package:smart_sensing_library/filter_tools.dart';
 
-/// Enum that represents ever [FilterOption] of [FilterTools].
+/// Enum representing each [FilterOption] of [FilterTools].
 ///
 /// Has a [axisNumber] that represents the number of axes that can be chosen.
 /// Has [shortText] that returns the filter as a [String].
 enum FilterOption {
-  /// Represents the maximum filter from [FilterTools].
+  /// Represents the maximum filter from [FilterTools.getMax].
   ///
   /// Axis number is 3.
   max(axisNumber: 3, shortText: "Max"),
 
-  /// Represents the minimum filter from [FilterTools].
+  /// Represents the minimum filter from [FilterTools.getMin].
   ///
   /// Axis number is 3.
   min(axisNumber: 3, shortText: "Min"),
 
-  /// Represents the average filter from [FilterTools].
+  /// Represents the average filter from [FilterTools.getAvg].
   ///
   /// Axis number is 1.
   avg(axisNumber: 1, shortText: "Avg"),
 
-  /// Represents the standard deviation filter from [FilterTools].
+  /// Represents the standard deviation filter from [FilterTools.getSD].
   ///
   /// Axis number is 1.
   sd(axisNumber: 1, shortText: "Standard dev."),
 
-  /// Represents the mode filter from [FilterTools].
+  /// Represents the mode filter from [FilterTools.getMode].
   ///
   /// Axis number is 3.
   mode(axisNumber: 3, shortText: "Mode"),
 
-  /// Represents the range filter from [FilterTools].
+  /// Represents the range filter from [FilterTools.getRange].
   ///
   /// Axis number is 1.
   range(axisNumber: 1, shortText: "Range"),
 
-  /// Represents the median filter from [FilterTools].
+  /// Represents the median filter from [FilterTools.getMedian].
   ///
   /// Axis number is 1.
   median(axisNumber: 1, shortText: "Median"),
 
-  /// Represents the sum filter from [FilterTools].
+  /// Represents the sum filter from [FilterTools.getSum].
   ///
   /// Axis number is 1.
   sum(axisNumber: 1, shortText: "Sum"),
