158afff8a4c2911cfe0afaacca1b2fc75893eec0
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2023-05-24 11:09:17 +0200

    docs: updated docs.

--- lib/multi_filter_tools.dart
+++ lib/multi_filter_tools.dart
@@ -4,91 +4,94 @@ import 'package:sensing_plugin/sensing_plugin.dart';
 
 import 'filter_tools.dart';
 
-///Extended class of [FilterTools].
+/// Extended class of [FilterTools].
 ///
-///This class is used for multi filtering requests.
-///Uses [FilterTools] as its base and querys the request on all given
-///[SensorId]s. Returns a [Map] with [SensorId] as the key and the queried
-///data as the values.
+/// This class is used for multi filtering requests.
+/// Uses [FilterTools] as its base and querys the request on all given
+/// [SensorId]s. Returns a [Map] with [SensorId] as the key and the queried
+/// data as the values.
 class MultiFilterTools {
-  final Map<SensorId, FilterTools?> _querryMap = HashMap();
+  final Map<SensorId, FilterTools?> _queryMap = HashMap();
 
-  ///Constructor for [MultiFilterTools].
+  /// Constructor for [MultiFilterTools].
   ///
-  ///All [List]s in [querryMap] get converted to a [FilterTools] instance.
-  ///The internal [_querryMap] is used to map all filter options
-  ///with the instances.
-  MultiFilterTools(Map<SensorId, FilterTools?> querryMap) {
-    _querryMap.addAll(querryMap);
+  /// Adds all values of [queryMap] to internal [_queryMap]
+  /// The internal [_queryMap] is used to map all filter options
+  /// with the instances.
+  MultiFilterTools(Map<SensorId, FilterTools?> queryMap) {
+    _queryMap.addAll(queryMap);
   }
 
-  ///Maps maximum of each [List] in given [interval] from [axis].
+  /// Applies [FilterTools.getMax] on every [SensorId]
+  /// in given [interval] from [axis].
   void getMax({Duration interval = Duration.zero, int axis = 0}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getMax(interval: interval, axis: axis);
     }
   }
 
-  ///Maps minimum of each [List] in given [interval] from [axis].
+  /// Applies [FilterTools.getMin] on every [SensorId]
+  /// in given [interval] from [axis].
   void getMin({Duration interval = Duration.zero, int axis = 0}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getMin(interval: interval, axis: axis);
     }
   }
 
-  ///Maps average of each [List] in given [interval].
+  /// Applies [FilterTools.getAvg] on every [SensorId].
   void getAvg({Duration interval = Duration.zero}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getAvg(interval: interval);
     }
   }
 
-  ///Maps amount of entries in each [List] in given [interval].
+  /// Applies [FilterTools.getCount] on every [SensorId] and
+  /// returns a corresponding [List].
   Map<SensorId, List<int>> getCount({Duration interval = Duration.zero}) {
     var countMap = HashMap<SensorId, List<int>>();
-    for (var key in _querryMap.keys) {
-      countMap[key] = _querryMap[key]!.getCount(interval: interval);
+    for (var key in _queryMap.keys) {
+      countMap[key] = _queryMap[key]!.getCount(interval: interval);
     }
     return countMap;
   }
 
-  ///Maps amount of entries in each [List] in given [interval].
+  /// Applies [FilterTools.getMedian] on every [SensorId].
   void getMedian({Duration interval = Duration.zero}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getMedian(interval: interval);
     }
   }
 
-  ///Maps amount of entries in [List] in given [interval].
+  /// Applies [FilterTools.getMode] on every [SensorId]
+  /// in given [interval] from [axis].
   void getMode({Duration interval = Duration.zero, int axis = 0}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getMode(interval: interval, axis: axis);
     }
   }
 
-  ///Maps the difference between the highest and lowest value in each [List]
-  ///in given [interval].
+   /// Applies [FilterTools.getRange] on every [SensorId].
   void getRange({Duration interval = Duration.zero}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getRange(interval: interval);
     }
   }
 
-  ///Maps the standard deviation of each [List] in given [interval].
+   /// Applies [FilterTools.getSD] on every [SensorId].
   void getSD({Duration interval = Duration.zero}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getSD(interval: interval);
     }
   }
 
-  ///Maps sum of each [List] in given [interval].
+  /// Applies [FilterTools.getSum] on every [SensorId].
   void getSum({Duration interval = Duration.zero}) {
-    for (var filter in _querryMap.values) {
+    for (var filter in _queryMap.values) {
       filter?.getSum(interval: interval);
     }
   }
 
-  ///Returns result of querry.
+  ///Returns result of query.
   Map<SensorId, List<SensorData>?> result() =>
-      _querryMap.map((key, value) => MapEntry(key, value?.result()));
+      _queryMap.map((key, value) => MapEntry(key, value?.result()));
 }
